91353b9870ae48e0f885461e31c798f6df12985d
Sayon Palit <sayon.palit@gmail.com>
2020-07-01 23:35:58 +0530

    Initial Commit
    -add makefiles
    -add source files
    -add header files

---
+++ src/kernel.c
@@ -0,0 +1,12 @@
+#include "../include/kb.h"
+#include "../include/isr.h"
+#include "../include/idt.h"
+#include "../include/util.h"
+#include "../include/shell.h"
+kmain()
+{
+	isr_install();
+	clearScreen();
+	print("Welcome to PALITOS \nPlease enter a command\n");
+    launch_shell(0);    
+}
